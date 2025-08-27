db051af70ca22afdf6dd766ed9cc1ef7de11e42e
u0604765262-hash <u0604765262@gmail.com>
2025-08-27 19:23:57 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,18 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("vitres-store").then((cache) =>
-      cache.addAll(["/", "/index.html", "/manifest.json"])
-    )
-  );
+const CACHE_NAME = "vitre-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "resume.html",
+  "manifest.json",
+  "logo.png",
+  "icon-192.png",
+  "icon-512.png",
+  "favicon.ico"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
 });
