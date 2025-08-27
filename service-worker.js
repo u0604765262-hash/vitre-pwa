7798fa0b0a5bb74da0cfeb15c759895ca14c547f
u0604765262-hash <u0604765262@gmail.com>
2025-08-27 22:14:11 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,39 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("app-cache").then(cache => {
-      return cache.addAll(["/", "/index.html", "/resume.html", "/manifest.json", "/logo.png"]);
+const CACHE_NAME = "bulle-savon-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./resume.html",
+  "./logo.png",
+  "./manifest.json"
+];
+
+// Installation : mettre les fichiers en cache
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+// Activation : nettoyage des anciens caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Fetch : sert depuis le cache d'abord
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
